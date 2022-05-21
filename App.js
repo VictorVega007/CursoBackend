a48628f1c8e512a8269a1104f82685be10af125b
victorv <victorvega.v@gmail.com>
2022-05-21 15:02:42 -0400

    Desafío 1

---
+++ App.js
@@ -0,0 +1,53 @@
+'use strict'
+
+class User {
+    constructor (name, lastname) {
+      this.name = name,
+      this.lastname = lastname,
+      this.pets = [],
+      this.books = []
+    };
+  
+  getFullName() {
+      return console.log(`Nombre de usuario: ${this.name} ${this.lastname}`);
+  };
+  
+  addPets(...petName) {
+      return this.pets.push(...petName);
+  };
+    
+  countPets() {
+      let numOfPets = this.pets.length;
+      return console.log(numOfPets);
+  };
+
+  addBooks(...book) {
+      return  this.books.push(...book);
+  };
+
+  getBookNames() {
+      let Array = this.books;
+      let newArray = [];
+      
+      Array.map(object => {
+          let nameOfBooks = object.nameBook;
+          
+          if(nameOfBooks) {
+            newArray.push(nameOfBooks);
+        };
+    });
+        console.log(newArray);
+  };
+};
+  
+  
+  const userOne = new User('Victor', 'Vega');
+  console.log(userOne);
+  userOne.getFullName();
+  userOne.addPets('Spike', 'Loki', 'Grumpy', 'Rocky', 'Raiden');
+  userOne.countPets();
+  userOne.addBooks({nameBook: 'Angels & Demons', author: 'Dan Brown'}, {nameBook: 'James Bond 007', author: 'Ian Fleming'});
+  userOne.getBookNames();
+
+
+  
