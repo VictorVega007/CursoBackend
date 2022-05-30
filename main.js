2a88148256eb0ebeef0ba20a8a47cc08a717ee84
victorv <victorvega.v@gmail.com>
2022-05-29 22:04:29 -0400

    Adding FS of Node

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+// const Container = require('./Container');
+
+// const newContainer = new Container('productos.txt');
+
+// const main = async () => {
+//     const product1 = await newContainer.save({ title: 'Iphone', price: 35.53})
+
+//     console.log(product1)
+
+//     const object2 = await newContainer.getById(2);
+//     console.log(object2); // { title: 'Goma', price: 57.75, id: 2 }
+
+//     await newContainer.deleteById(2);
+
+//     const allCurrentObjects = await newContainer.getAll();
+//     console.log(allCurrentObjects);
+// };
+
+// main();
+
+
